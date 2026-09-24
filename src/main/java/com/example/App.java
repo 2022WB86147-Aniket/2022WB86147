97aeb42c76cb99875d8811b9a875f81537e1cadc
Aniket Dwivedi <2022wb86147@wilp.bits-pilani.ac.in>
2026-09-24 18:25:13 +0530

    Modified source code for continuous integration

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Aniket, BITS ID: 2022WB86147");
+        System.out.println("Hello from Aniket, BITS ID: 2022WB86147, Continuous Integration");
     }
 }
